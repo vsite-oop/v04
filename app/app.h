a831cb97c0f347474984db45f3c31b735b063204
Ivor Novosel <ivnovosel@vsite.hr>
2022-11-13 14:31:36 +0100

    All tests passed + main DONE

--- app/app.h
+++ app/app.h
@@ -2,7 +2,62 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v4 
+namespace vsite::oop::v4
 {
+	class animal {
+	public:
+		virtual std::string species() const = 0;
+		virtual uint32_t legs() const = 0;
+		virtual ~animal() = 0;
+	};
 
+	class insect : public animal {
+		const uint32_t ins_legs = 6;
+	public:
+		uint32_t legs() const override;
+	};
+
+	class spider : public animal {
+		const uint32_t sp_legs = 8;
+	public:
+		uint32_t legs() const override;
+	};
+
+	class bird : public animal {
+		const uint32_t brd_legs = 2;
+	public:
+		uint32_t legs() const override;
+	};
+
+	class cockroach : public insect {
+		const std::string ins_name = "cockroach";
+		const uint32_t id = 1;
+	public:
+		std::string species() const override;
+	};
+
+	class tarantula : public spider {
+		const std::string spi_name = "tarantula";
+		const uint32_t id = 3;
+	public:
+		std::string species() const override;
+	};
+
+	class sparrow : public bird {
+		const std::string bir_name = "sparrow";
+		const uint32_t id = 2;
+	public:
+		std::string species() const override;
+	};
+
+	class leg_counter {
+		uint32_t nr_legs;
+	public:
+		leg_counter();
+		uint32_t legs() const;
+		std::string add_animal(const animal* a);
+
+	};
+
+	std::unique_ptr<animal> animal_factory(uint32_t id);
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,20 @@
 #include "app.h"
+#include <iostream>
 
 // read animal id's (until 0) and display species name
 // display total leg count
+using namespace vsite::oop::v4;
 int main()
 {
+	leg_counter lc;
+	uint32_t id = 1;
+
+	while (id) {
+		std::cout << "Enter animal ID (0 to terminate): ";
+		std::cin >> id;
+		if (id == 0) break;
+		lc.add_animal(animal_factory(id).get());
+		std::cout << "Species name: " << animal_factory(id).get()->species() << "\n";
+	}
+	std::cout << "Total leg count is: " << lc.legs();
 }
