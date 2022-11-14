ed75cbd84c7a48cea2da91a5c3380761888de67f
Filip Les <ketvok@gmail.com>
2022-11-14 15:15:16 +0100

    Sad sam iz klasa izbacio data membere za naziv vrste i broj nogu. Zanima me je li sad sve od ovoga što sam uspio riješiti ok? Što se tiče dijela s animal factory, ne razumijem tko bi trebao koga, što niti kako...

--- app/app.cpp
+++ app/app.cpp
@@ -4,6 +4,41 @@
 namespace vsite::oop::v4
 {
 	std::string cockroach::species() const {
+		return "cockroach";
+	}
+
+	std::string sparrow::species() const {
+		return "sparrow";
+	}
+
+	std::string tarantula::species() const {
+		return "tarantula";
+	}
+
+	unsigned insect::legs() const {
+		return 6;
+	}
+
+	unsigned bird::legs() const {
+		return 2;
+	}
+
+	unsigned spider::legs() const {
+		return 8;
+	}
+}
+
+
+
+
+
+/*OLD CODE
+#include "app.h"
+#include <algorithm>
+
+namespace vsite::oop::v4
+{
+	std::string cockroach::species() const {
 		return insect_name;
 	}
 
@@ -26,4 +61,4 @@ namespace vsite::oop::v4
 	unsigned spider::legs() const {
 		return spider::spider_legs;
 	}
-}
+}*/
--- app/app.h
+++ app/app.h
@@ -16,7 +16,87 @@ Bazna klasa je polimorfna ako ima bar jednu virtualnu funkciju.
 ----------------------------------------------------
 */
 
-namespace vsite::oop::v4 
+namespace vsite::oop::v4
+{
+	class animal {
+	public:
+		virtual std::string species() const = 0;
+		virtual unsigned legs() const = 0;
+	};
+
+
+	class insect : public animal {
+	public:
+		virtual unsigned legs() const;
+	};
+
+
+	class spider : public animal {
+	public:
+		virtual unsigned legs() const;
+	};
+
+
+	class bird : public animal {
+	public:
+		virtual unsigned legs() const;
+	};
+
+
+	class cockroach : public insect {
+		unsigned id = 1;
+	public:
+		virtual std::string species() const;  // Must copy pure virtual function in derived class!
+	};
+
+
+	class tarantula : public spider {
+		unsigned id = 3;
+	public:
+		virtual std::string species() const;
+	};
+
+
+	class sparrow : public bird {
+		unsigned id = 2;
+	public:
+		virtual std::string species() const;
+	};
+
+
+	class leg_counter {
+	public:
+		std::string add_animal();
+	};
+
+
+	//animal_factory(unsigned id);
+}
+
+
+
+
+
+/*OLD CODE
+#pragma once
+#include <string>
+#include <memory>
+
+/*Apstraktna klasa je ona koja ima bar jednu èisto virtualnu funkciju.
+To nije konkretna klasa. Ne možemo uopæe praviti objekt takve klase
+(kompajler ne da), nego se koriste pointeri i reference na njih.
+Pure virtual funkcija je ona koja se u izvedenoj klasi MORA PREPISATI.
+
+Polimorfizam je mehanizam kojim se dopušta da razne klase koriste
+ista imena za svoje metode.
+Bazna klasa je polimorfna ako ima bar jednu virtualnu funkciju.
+
+----------------------------------------------------
+ SVI INSEKTI IMAJU 8 NOGU, SVI PAUCI IMAJU 6 NOGU!!!
+----------------------------------------------------
+
+
+namespace vsite::oop::v4
 {
 	class animal {
 	public:
@@ -76,5 +156,6 @@ namespace vsite::oop::v4
 	};
 
 
-	//animal_factory(unsigned id);
-}
+	// animal_factory(unsigned id);
+}*/
+
