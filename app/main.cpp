2a5b45518c6e1abc1afdd1751dd8698b43c475a0
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-12 20:35:49 +0100

    vj 4 resolved

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,29 @@
 #include "app.h"
-
+#include <iostream>
 // read animal id's (until 0) and display species name
 // display total leg count
+
+using namespace::vsite::oop::v4;
 int main()
 {
+    leg_counter lc;
+    unsigned int ID = 1;
+
+    while (true) {
+        std::cout << "Unesite  ID zivotinje:" << "\n";
+        std::cin >> ID;
+        if (ID > 3) {
+            std::cout << "Wrong input.\n";
+            continue;
+        }
+
+        if (ID == 0) {
+           
+            break;
+        }
+        lc.add_animal(animal_factory(ID).get());
+        std::cout << "Vrsta: " << animal_factory(ID).get()->species() << "\n";
+
+    }
+    std::cout << "Ukupni broj nogu je: " << lc.legs();
 }
