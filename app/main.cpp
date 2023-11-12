c498bb362c23f41624ba4559e491fc91337823fa
Josip Grabušić <jograbus@vsite.hr>
2023-11-12 09:51:08 +0100

    Završen kod za vježbu 4

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,20 @@
 #include "app.h"
+#include <iostream>
 
-// read animal id's (until 0) and display species name
-// display total leg count
-int main()
-{
+using namespace vsite::oop::v4;
+
+int main() {
+    leg_counter broj_nogu;
+    unsigned int ID = 1;
+
+    while (ID) {
+        std::cout << "Upisi ID: ";
+        std::cin >> ID;
+        if (!ID) break;
+        broj_nogu.add_animal(animal_factory(ID).get());
+        std::cout << "Vrsta zivotinje: " << animal_factory(ID).get()->species() << "\n";
+    }
+
+    std::cout << "Ukupan broj nogu: " << broj_nogu.legs() << "\n";
+    return 0;
 }
