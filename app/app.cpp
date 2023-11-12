19644d38bd8492d312cdabaf0d6b5b4516529657
Tomislav Pavic <t.pavic@outlook.com>
2023-11-12 18:16:18 +0100

    vj 04 solved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,23 @@
 
 namespace vsite::oop::v4
 {
+    std::string leg_counter::add_animal(animal* a) {
+        legCount += (*a).legs();
+        return (*a).species();
+    }
 
+    std::unique_ptr<animal> animal_factory(int animalId)
+    {
+        switch (animalId)
+        {
+        case 1:
+            return std::make_unique<cockroach>();
+        case 2:
+            return std::make_unique<sparrow>();
+        case 3:
+            return std::make_unique<tarantula>();
+        default:
+            return nullptr;
+        }
+    };
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,27 @@
 #include "app.h"
+#include <iostream>
+
+using namespace vsite::oop::v4;
 
 // read animal id's (until 0) and display species name
 // display total leg count
 int main()
 {
+	int animalId = 0;
+
+	leg_counter lc;
+
+	while (true)
+	{
+		std::cout << "Odaberite zivinu 1-3 ili 0 za izlaz: ";
+		std::cin >> animalId;
+
+		if (animalId < 1 || animalId > 3)
+		{
+			break;
+		}
+
+		std::cout << "odabrali ste: " << lc.add_animal(animal_factory(animalId).get()) << "\n";
+	}
+	std::cout << "suma nogu svih zivina jest: " << lc.legs() << "\n\n";
 }
