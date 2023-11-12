6a0ab0e8c9639a0a34854bc830eaab686844888f
Luka Blagojević <lublagoj@vsite.hr>
2023-11-12 19:23:24 +0100

    v04

--- app/app.h
+++ app/app.h
@@ -2,7 +2,41 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v4 
+namespace vsite::oop::v4
 {
+    class animal {
+    public:
+        virtual ~animal() = default;
+        virtual std::string species() const = 0;
+        virtual unsigned int legs() const = 0;
+    };
 
+    class insect : public animal {
+        unsigned int legs() const override { return 6u; }
+    };
+    class cockroach : public insect {
+        std::string species() const override { return "cockroach"; }
+    };
+    class spider : public animal {
+        unsigned int legs() const override { return 8u; }
+    };
+    class tarantula : public spider {
+        std::string species() const override { return "tarantula"; }
+    };
+
+    class bird : public animal {
+        unsigned int legs() const override { return 2u; }
+    };
+    class sparrow : public bird {
+        std::string species() const override { return "sparrow"; }
+    };
+
+    class leg_counter {
+        unsigned legCount = 0;
+    public:
+        std::string add_animal(animal* a);
+        unsigned legs() const { return legCount; }
+    };
+
+    std::unique_ptr<animal> animal_factory(int animalId);
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,28 @@
 #include "app.h"
+#include <iostream>
+
+using namespace vsite::oop::v4;
 
 // read animal id's (until 0) and display species name
 // display total leg count
 int main()
 {
+    int IDzivotinje = 0;
+    std::cout << "Unesite redni broj zivotinje (1-3): \n";
+
+    leg_counter bn;
+
+    while (true)
+    {
+        std::cout << "Odabir: ";
+        std::cin >> IDzivotinje;
+
+        if (IDzivotinje < 1 || IDzivotinje > 3)
+        {
+            break;
+        }
+
+        std::cout << "Zivotinja: " << bn.add_animal(animal_factory(IDzivotinje).get()) << "\n";
+    }
+    std::cout << "Ukupan broj nogu: " << bn.legs() << "\n\n";
 }
