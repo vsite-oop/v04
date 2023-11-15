6b90a8f987a7bdac1aa205a1dff8084c110d145f
Marko Pavić <mapavic@vsite.hr>
2023-11-15 22:24:30 +0100

    Ispravljen kod

--- test/test.cpp
+++ test/test.cpp
@@ -66,7 +66,7 @@ namespace all_tests
 			Assert::AreEqual("sparrow", lc.add_animal(animal_factory(2).get()).c_str());
 			Assert::AreEqual("tarantula", lc.add_animal(animal_factory(3).get()).c_str());
 			Assert::AreEqual(16u, lc.legs());
-		}
+		}	
 
 		TEST_METHOD(legg_counter_same_animal)
 		{
